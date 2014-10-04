05bc21b1d17f076d97f3bb05e8531cf8ff767aea
Federico Curzel <cuuuurzel@gmail.com>
2014-10-04 16:32:16 +0200

    Working damn well

---
+++ com/cuuuurzel/onScreenViews/OnScreenView.java
@@ -0,0 +1,225 @@
+package com.cuuuurzel.onScreenViews;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.cuuuurzel.utils.MyUtils;
+
+/**
+ * Use this class to manage on-screen dialogs, just like facebook chat-heads.
+ * There is an integrate gesture listener, so you don't have to care.
+ */
+public class OnScreenView extends View {
+
+    /**
+     * Current direction [ dx, dy ], (both between 0 and 1).
+     */
+    public float[] direction;
+
+    /**
+     * View movement speed.
+     */
+    public float mSpeed;
+
+    /**
+     * Size of the view in pixels.
+     */
+    public int mWidth, mHeight;
+
+    /**
+     * Layout params for the window manager
+     */
+    protected WindowManager.LayoutParams mLayoutParams;
+
+    /**
+     * If you need to dismiss the view outside use this value.
+     */
+    public boolean toRemove;
+
+    /**
+     * Use this to dynamically attach view to the main one.
+     */
+    protected LayoutInflater layoutInflater;
+
+    public OnScreenView( Context c, int w, int h ) {
+        super( c );
+
+        isVisible = true;
+        toRemove  = false;
+        direction = new float[]{ 0f, 0f };
+
+        setupLayoutThings( w, h );
+        setupGestureListener();
+        setupInitialViewLayout();
+    }
+
+    public boolean onClick( MotionEvent me ) {
+        Toast.makeText( getContext(), "View clicked!", Toast.LENGTH_SHORT ).show();
+        return true;
+    }
+
+    public boolean onFling( MotionEvent m1, MotionEvent m2, float dx, float dy ) {
+        Toast.makeText( getContext(), "Fling!", Toast.LENGTH_SHORT ).show();
+        return true;
+    }
+
+    /**
+     * Changes directions and moves the view.
+     * Will also change animations.
+     */
+    public void update( long dt ) {
+
+        float dx = this.direction[0] * mSpeed * dt;
+        float dy = this.direction[1] * mSpeed * dt;
+        float w = MyUtils.screenW(getContext());
+        //float h = MyUtils.screenH( getContext() );
+
+        if ( this.getX()+dx <= -w/2 || this.getX()+dx+this.getWidth() >= w/2 ) {
+            onScreenBoundReached();
+        } else {
+            this.moveTo( this.getX()+dx, this.getY()+dy );
+        }
+
+        //Finally, commit changes to the view
+        updateLayout();
+    }
+
+    /**
+     * Will be called when the view touches the screen bounds.
+     * If the view is moving, you may change direction here for bouncing effects.
+     */
+    protected void onScreenBoundReached() {
+        Toast.makeText( getContext(), "Screen bound reached...", Toast.LENGTH_SHORT ).show();
+    }
+
+    /**
+     * Will load the initial layout.
+     */
+    protected void setupInitialViewLayout() {
+        this.setBackgroundColor(0xA0FFFFFF);
+    }
+
+    /**
+     * Will remove the view from the window manager.
+     */
+    public void dismiss() {
+        if ( !isVisible ) { return; }
+        Context c = getContext();
+        WindowManager wm = ( WindowManager ) c.getSystemService( c.WINDOW_SERVICE );
+        wm.removeView(this);
+        isVisible = false;
+    }
+
+    /**
+     * Will add the view to the window manager.
+     */
+    public void show() {
+        if ( isVisible ) { return; }
+        Context c = getContext();
+        WindowManager wm = ( WindowManager ) c.getSystemService( c.WINDOW_SERVICE );
+        wm.addView(this, this.getLayoutParams());
+        isVisible = true;
+    }
+
+    /**
+     * Shows if hidden, hides if shown.
+     */
+    public void toogleShow() {
+        if ( isVisible ) dismiss(); else show();
+    }
+
+    /**
+     * Use this to add an arbitrary view to the root.
+     */
+    protected void addViewToRoot( View v ) {
+        ( ( ViewGroup ) this.getParent() ).addView(
+                v,
+                new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                )
+        );
+    }
+
+//Less important thigs
+
+    private boolean isVisible;
+    private WindowManager mWindowManager;
+    private GestureDetector mGestureDetector;
+
+    private final void moveTo( float x, float y ) {
+        this.setX( x );
+        this.setY(y);
+    }
+
+    private void setupGestureListener() {
+
+        GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
+
+            @Override
+            public boolean onSingleTapUp( MotionEvent me ) {
+                return OnScreenView.this.onClick( me );
+            }
+
+            @Override
+            public boolean onFling( MotionEvent m1, MotionEvent m2, float dx, float dy ) {
+                return OnScreenView.this.onFling( m1, m2, dx, dy );
+            }
+        };
+
+        mGestureDetector = new GestureDetector( getContext(), gestureListener );
+        setOnTouchListener(
+                new View.OnTouchListener() {
+
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        mGestureDetector.onTouchEvent(motionEvent);
+                        return true;
+                    }
+                }
+        );
+    }
+    public final WindowManager.LayoutParams getLayoutParams() {
+        return mLayoutParams;
+    }
+
+    private final void updateLayout() {
+        mLayoutParams.x = (int) this.getX();
+        mLayoutParams.y = (int) this.getY();
+
+        mWindowManager.updateViewLayout(
+                this, mLayoutParams
+        );
+    }
+
+    private final void setupLayoutThings( int w, int h ) {
+
+        Context c = getContext();
+
+        mWidth    = w;
+        mHeight   = h;
+        mSpeed = 1;
+
+        layoutInflater = ( LayoutInflater ) c.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+        mWindowManager = ( WindowManager ) c.getSystemService( Context.WINDOW_SERVICE );
+
+        mLayoutParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+        mLayoutParams.width = mWidth;
+        mLayoutParams.height = mHeight;
+
+        this.moveTo( MyUtils.screenW(c)/2, MyUtils.screenH( c )/2 );
+    }
+
+    public boolean isVisible() { return isVisible; }
+}
---
+++ com/cuuuurzel/onScreenViews/OnScreenViewsManagementService.java
@@ -0,0 +1,82 @@
+package com.cuuuurzel.onScreenViews;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+
+import java.util.ArrayList;
+
+/**
+ * Use this class to manage you on-screen views.
+ */
+public class OnScreenViewsManagementService extends Service {
+
+    /**
+     * Animation's FPS.
+     */
+    public static float FPS = 30;
+
+    /**
+     * Used to post the update callback.
+     */
+    public static int DT = ( int )( 1000 / FPS );
+
+    /**
+     * Used to post updates.
+     */
+    private Handler mHandler;
+
+    /**
+     * List of attached views.
+     */
+    protected ArrayList< OnScreenView > views;
+
+    /**
+     * Used to update all the views.
+     */
+    public Runnable updateCallback = new Runnable() {
+
+        public void run() {
+
+            //Get milliseconds from last update
+            currentTime = System.currentTimeMillis();
+            long dt = currentTime - lastUpdateTime;
+
+            //Update all visible views
+            for ( OnScreenView view : views ) {
+                if ( view.isVisible() ) { view.update( dt ); }
+                if ( view.toRemove ) { view.dismiss(); }
+            }
+
+            //Get ready for next update
+            lastUpdateTime = currentTime;
+            scheduleUpdate();
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mHandler = new Handler();
+        views = new ArrayList<OnScreenView>();
+        createInitialViews();
+        scheduleUpdate();
+    }
+
+    /**
+     * Called by the constructor to create the first views.
+     */
+    protected void createInitialViews() {}
+
+//Less important things
+
+    private void scheduleUpdate() {
+        mHandler.postDelayed( updateCallback, DT );
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+
+    private long lastUpdateTime, currentTime;
+}
